fc7b1629b12c3864672be3db8cd6fa8dc5710362
Kai Paule <kai@paule-online.de>
2021-04-02 14:30:40 +0200

    add basic bootstrap nav with jinja2

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+@app.route('/index/')
+@app.route('/home/')
+def home():
     return render_template("index.html")
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
