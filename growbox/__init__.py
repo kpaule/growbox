278920f6762155e67281d6ed9523b6477a7b939c
Kai Paule <kai@paule-online.de>
2021-04-06 14:09:16 +0200

    add login form

--- growbox/__init__.py
+++ growbox/__init__.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "key_for_development")
 
 from growbox import routes
--- growbox/routes.py
+++ growbox/routes.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from flask.helpers import url_for
 from growbox import app
+from growbox.forms import LoginForm
 
 @app.route('/')
 @app.route('/index/')
 @app.route('/home/')
 def home():
     return render_template("index.html")
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Hello {form.username.data}! You are now logged in.", "success")
+        return redirect(url_for("dashboard"))
+    return render_template("login.html", form=form)
 
 @app.route('/dashboard/')
 def dashboard():
