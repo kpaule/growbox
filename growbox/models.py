85d002b32a30c3c6d79cfee81d6d120b282c3d48
Kai Paule <kai@paule-online.de>
2021-04-08 08:16:19 +0200

    add db models for temperature and humidity

--- growbox/models.py
+++ growbox/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask_login import UserMixin
 
 from growbox import db, login_manager
@@ -11,6 +13,27 @@ class User(db.Model, UserMixin):
     def __repr__(self) -> str:
         return f"User('{self.username}', '{self.password}')"
 
+
 @login_manager.user_loader
 def load_user(user_id: int) -> User:
     return User.query.get(user_id)
+
+
+class Temperature(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(100), nullable=False)
+    temperature = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Temperature('{self.topic}', '{self.temperature}', '{self.date}')"
+
+
+class Humidity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(100), nullable=False)
+    humidity = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Humidity('{self.topic}', '{self.humidity}', '{self.date}')"
