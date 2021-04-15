567e186fbf4d1f594f3499b7669bb3bb04b2d99c
Kai Paule <kai@paule-online.de>
2021-04-15 11:27:44 +0200

    improving frontend, adding placeholder content

--- growbox/home/routes.py
+++ growbox/home/routes.py
@@ -4,6 +4,17 @@ from flask_login import login_required
 home = Blueprint("home", __name__)
 
 @home.route('/dashboard/')
+@home.route('/dashboard/overview/')
 @login_required
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard_overview.html")
+
+@home.route('/dashboard/temperature/')
+@login_required
+def dashboard_temperature():
+    return render_template("dashboard_temperature.html")
+
+@home.route('/dashboard/humidity/')
+@login_required
+def dashboard_humidity():
+    return render_template("dashboard_humidity.html")
