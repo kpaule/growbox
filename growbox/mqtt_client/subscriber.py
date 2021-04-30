c8a677871078d53ca7f071feacb6884e3fae0462
Kai Paule <kai@paule-online.de>
2021-04-30 11:52:54 +0200

    fix mqtt subscription for sub-topics not working

--- growbox/mqtt_client/subscriber.py
+++ growbox/mqtt_client/subscriber.py
@@ -1,6 +1,7 @@
+import json
 import logging
-from flask import Blueprint
 
+from flask import Blueprint
 from growbox import mqtt
 
 mqtt_client = Blueprint("mqtt_client", __name__)
@@ -11,47 +12,33 @@ def handle_connect(client, userdata, flags, rc):
     if rc != 0:
         logging.error(f"Connection to broker failed with code: {rc}")
     else:
-        mqtt.subscribe("growboxhsalbsensors/#")
+        mqtt.subscribe("growboxhsalbsensors")
 
 
-@mqtt.on_topic("growboxhsalbsensors/Bodenfeucht")
-def handle_topic_bodenfeucht(client, userdata, message):
-    print(f"Received message on topic {message.topic}: {message.payload.decode()}")
+@mqtt.on_message()
+def handle_message(client, userdata, message):
+    print(f"Received message {message.payload.decode()}")
+    data = json.loads(message.payload)
+    
+    for key, value in data.items():
+        print(f"{key}: {value}")
 
-
-@mqtt.on_topic("growboxhsalbsensors/Feuchwand")
-def handle_topic_bodenfeucht(client, userdata, message):
-    print(f"Received message on topic {message.topic}: {message.payload.decode()}")
-
-
-@mqtt.on_topic("growboxhsalbsensors/Tempwand")
-def handle_topic_bodenfeucht(client, userdata, message):
-    print(f"Received message on topic {message.topic}: {message.payload.decode()}")
-
-
-@mqtt.on_topic("growboxhsalbsensors/TempBoden")
-def handle_topic_bodenfeucht(client, userdata, message):
-    print(f"Received message on topic {message.topic}: {message.payload.decode()}")
-
-
-@mqtt.on_topic("growboxhsalbsensors/LED")
-def handle_topic_bodenfeucht(client, userdata, message):
-    print(f"Received message on topic {message.topic}: {message.payload.decode()}")
-
-
-@mqtt.on_topic("growboxhsalbsensors/Luefter")
-def handle_topic_bodenfeucht(client, userdata, message):
-    print(f"Received message on topic {message.topic}: {message.payload.decode()}")
-
-
-@mqtt.on_topic("growboxhsalbsensors/Pumpe")
-def handle_topic_bodenfeucht(client, userdata, message):
-    print(f"Received message on topic {message.topic}: {message.payload.decode()}")
-
-
-@mqtt.on_topic("growboxhsalbsensors/Heizung")
-def handle_topic_bodenfeucht(client, userdata, message):
-    print(f"Received message on topic {message.topic}: {message.payload.decode()}")
+        if key == "Bodenfeucht":
+            pass
+        elif key == "Feuchwand":
+            pass
+        elif key == "Tempwand":
+            pass
+        elif key == "TempBoden":
+            pass
+        elif key == "LED":
+            pass
+        elif key == "Luefter":
+            pass
+        elif key == "Pumpe":
+            pass
+        elif key == "Heizung":
+            pass
 
 # For debugging
 # @mqtt.on_log()
