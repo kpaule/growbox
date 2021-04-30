1661f7344b0eb278510ae243188856a642fb623a
Kai Paule <kai@paule-online.de>
2021-04-30 11:51:47 +0200

    update test publisher

--- fake_growbox_publisher.py
+++ fake_growbox_publisher.py
@@ -13,6 +13,10 @@ client.connect("broker.hivemq.com", 1883)
 client.loop_start()
 
 while True:
+    #client.publish("growboxhsalb", "{\"LED\":\"off\"}")
+    #client.publish("growboxhsalb", "{\"Pumpe\":\"off\"}")
+    #client.publish("growboxhsalb", "{\"Heizung\":\"off\"}")
+    #client.publish("growboxhsalb", "{\"Luefter\":\"off\"}")
     client.publish("growboxhsalbsensors/Bodenfeucht", random.uniform(1.5, 1.9), 0)
     client.publish("growboxhsalbsensors/Feuchwand", random.uniform(1.5, 1.9), 0)
     client.publish("growboxhsalbsensors/Tempwand", random.uniform(15.0, 16.0), 0)
