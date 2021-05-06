ad97b6c28f4aa475576ad5e57242bc395ffa39c8
Kai Paule <kai@paule-online.de>
2021-05-06 10:57:45 +0200

    merge metric models in one

--- growbox/home/routes.py
+++ growbox/home/routes.py
@@ -1,4 +1,4 @@
-from growbox.models import HumidityFloor, HumidityWall, TemperatureFloor, TemperatureWall
+from growbox.models import Metric
 from flask import Blueprint, render_template
 from flask_login import login_required
 
@@ -9,27 +9,11 @@ home = Blueprint("home", __name__)
 @home.route('/dashboard/overview/')
 @login_required
 def dashboard():
-    temp_floor = TemperatureFloor.query.all()
-    temp_floor_data = [x.temperature for x in temp_floor]
-    temp_floor_date = [x.date.strftime("%A, %d. %B %Y %I:%M%p") for x in temp_floor]
-    temp_wall = TemperatureWall.query.all()
-    temp_wall_data = [x.temperature for x in temp_wall]
-    temp_wall_date = [x.date.strftime("%A, %d. %B %Y %I:%M%p") for x in temp_wall]
-    humi_floor = HumidityFloor.query.all()
-    humi_floor_data = [x.humidity for x in humi_floor]
-    humi_floor_date = [x.date.strftime("%A, %d. %B %Y %I:%M%p") for x in humi_floor]
-    humi_wall = HumidityWall.query.all()
-    humi_wall_data = [x.humidity for x in humi_wall]
-    humi_wall_date = [x.date.strftime("%A, %d. %B %Y %I:%M%p") for x in humi_wall]
+    metrics = Metric.query.all()
+    metrics_date = [x.date.strftime("%A, %d. %B %Y %I:%M%p") for x in metrics]
     return render_template("dashboard_overview.html",
-                           temp_floor_data=temp_floor_data,
-                           temp_floor_date=temp_floor_date,
-                           temp_wall_data=temp_wall_data,
-                           temp_wall_date=temp_wall_date,
-                           humi_floor_data=humi_floor_data,
-                           humi_floor_date=humi_floor_date,
-                           humi_wall_data=humi_wall_data,
-                           humi_wall_date=humi_wall_date)
+                           metrics=metrics,
+                           metrics_date=metrics_date)
 
 
 @home.route('/dashboard/temperature/')
