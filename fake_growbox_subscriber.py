ad9d14f56e7ef9c3ff80fe3509d957861f99199d
mien <mienth2@gmail.com>
2021-05-06 17:43:07 +0200

    add fake subscriber

---
+++ fake_growbox_subscriber.py
@@ -0,0 +1,19 @@
+import random, time, json
+
+import paho.mqtt.client as paho
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("growboxhsalb/#")
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
+client = paho.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("broker.hivemq.com", 1883, 60)
+client.loop_forever()
