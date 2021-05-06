d921fa8b3746452e94170166cf8067eacc126806
mien <mienth2@gmail.com>
2021-05-06 17:48:13 +0200

    add route commands

--- growbox/home/routes.py
+++ growbox/home/routes.py
@@ -1,12 +1,16 @@
+import json
 from datetime import datetime, timedelta
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from flask_login import login_required
 from sqlalchemy import desc
 
+from growbox import create_app, mqtt
 from growbox.models import Metric
 
 home = Blueprint("home", __name__)
+with create_app().app_context():
+    topic_ctrl = current_app.config["MQTT_TOPIC_CTRL"]
 
 
 @home.route('/dashboard/')
@@ -31,3 +35,43 @@ def dashboard_time(time, amount):
     for i, m in enumerate(metrics):
         result[i] = m.as_dict()
     return result
+
+
+@home.route("/dashboard/led/<int:status>/", methods=["GET", "POST"])
+@login_required
+def toggle_led(status):
+    if status == 1:
+        mqtt.publish(topic_ctrl, "{\"LED\":\"on\"}")
+    else:
+        mqtt.publish(topic_ctrl, "{\"LED\":\"off\"}")
+    return '', 200
+
+
+@home.route("/dashboard/pump/<int:status>/", methods=["GET", "POST"])
+@login_required
+def toggle_pump(status):
+    if status == 1:
+        mqtt.publish(topic_ctrl, "{\"Pumpe\":\"on\"}")
+    else:
+        mqtt.publish(topic_ctrl, "{\"Pumpe\":\"off\"}")
+    return '', 200
+
+
+@home.route("/dashboard/heating/<int:status>/", methods=["GET", "POST"])
+@login_required
+def toggle_heating(status):
+    if status == 1:
+        mqtt.publish(topic_ctrl, "{\"Heizung\":\"on\"}")
+    else:
+        mqtt.publish(topic_ctrl, "{\"Heizung\":\"off\"}")
+    return '', 200
+
+
+@home.route("/dashboard/fan/<int:status>/", methods=["GET", "POST"])
+@login_required
+def toggle_fan(status):
+    if status == 1:
+        mqtt.publish(topic_ctrl, "{\"Luefter\":\"on\"}")
+    else:
+        mqtt.publish(topic_ctrl, "{\"Luefter\":\"off\"}")
+    return '', 200
--- growbox/mqtt_client/publisher.py
+++ growbox/mqtt_client/publisher.py
@@ -4,32 +4,32 @@ from flask import current_app
 from growbox import create_app, mqtt
 
 with create_app().app_context():
-    topic_ctrl = current_app.config["MQTT_TOPIC"]
+    topic_ctrl = current_app.config["MQTT_TOPIC_CTRL"]
 
 
-def set_led(status: bool):
-    if status:
-        mqtt.publish(topic_ctrl, json.dumps({"LED": "on"}))
+def set_led(status: int):
+    if status == 1:
+        mqtt.publish(topic_ctrl, "{\"LED\":\"on\"}")
     else:
-        mqtt.publish(topic_ctrl, json.dumps({"LED": "off"}))
+        mqtt.publish(topic_ctrl, "{\"LED\":\"off\"}")
 
 
-def set_fan(status: bool):
-    if status:
-        mqtt.publish(topic_ctrl, json.dumps({"Luefter": "on"}))
+def set_fan(status: int):
+    if status == 1:
+        mqtt.publish(topic_ctrl, "{\"Luefter\":\"on\"}")
     else:
-        mqtt.publish(topic_ctrl, json.dumps({"Luefter": "off"}))
+        mqtt.publish(topic_ctrl, "{\"Luefter\":\"off\"}")
 
 
-def set_pump(status: bool):
-    if status:
-        mqtt.publish(topic_ctrl, json.dumps({"Pumpe": "on"}))
+def set_pump(status: int):
+    if status == 1:
+        mqtt.publish(topic_ctrl, "{\"Pumpe\":\"on\"}")
     else:
-        mqtt.publish(topic_ctrl, json.dumps({"Pumpe": "off"}))
+        mqtt.publish(topic_ctrl, "{\"Pumpe\":\"off\"}")
 
 
-def set_heating(status: bool):
-    if status:
-        mqtt.publish(topic_ctrl, json.dumps({"Heizung": "on"}))
+def set_heating(status: int):
+    if status == 1:
+        mqtt.publish(topic_ctrl, "{\"Heizung\":\"on\"}")
     else:
-        mqtt.publish(topic_ctrl, json.dumps({"Heizung": "off"}))
+        mqtt.publish(topic_ctrl, "{\"Heizung\":\"off\"}")
